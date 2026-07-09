bbe20141c0675383c58acb73923abe5e2cd86411
Vivek617 <vivekreddykeesara@gmail.com>
2026-07-09 15:52:54 +0530

    Add files via upload

---
+++ Tasks8.py
@@ -0,0 +1,22 @@
+
+
+#Create a class Person with a private attribute __name. Provide methods to set and get the private attribute.
+
+class person:
+    def __init__(self):
+        self.__name == ""
+    def set__name(self, name):
+        self.__name = name
+    
+
+
+
+
+
+
+
+
+
+
+
+
